c627bcf293f3a4eb8484cf165af7154d8a1a997d
franrg19 <franrg19@outlook.com>
2025-03-30 10:23:20 +0200

    segundo ejercicio de practicas git

---
+++ Practicas_git_v2.py
@@ -0,0 +1,9 @@
+#PRACTICAS GIT - EJERCICIO 2
+
+#Crea nuevo repositorio remoto
+#Copia la URL del repositorio remoto
+#Configura el repositorio remoto en local
+#Verifica que el repositorio remoto se ha agregado correctamente
+#Renombra la rama principal
+#Haz push de tu commit inicial al repo remoto
+#Verifica que el push se ha realizado correctamente.
