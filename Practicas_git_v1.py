5bbb82cf742bb511d5c734cdecfdff833b208420
franrg19 <franrg19@outlook.com>
2025-03-30 10:13:44 +0200

    primer commit de mi primera practica

---
+++ Practicas_git_v1.py
@@ -0,0 +1,8 @@
+#INTRODUCION A GIT - PRACTICA 1
+
+# Crea un nuevo repositorio local
+# Navega al directorio del repositorio
+# Crea un archivo README.md
+# Agrega el archivo al área de preparación
+# Realiza un commit con el mensaje "commit inicial"
+# Visualiza el historial de commits
